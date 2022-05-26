3126ab90117ec0c3bbe8c018157aab78fd6ba20d
Sihang Qiu <qiusihang@gmail.com>
2022-05-26 09:52:26 +0800

    IE compatible

--- files/header.js
+++ files/header.js
@@ -1,30 +1,30 @@
-document.getElementById("header").innerHTML = `
-		<div class="header" style="padding-top:20px; padding-bottom:5px; background:#d2d2d2">
-			<!-- <img id="header-img" src="" width="800px"> -->
-			<center>
-				<img id="profile-img" src="./files/img/profile.jpg"></img>
-				<h1><strong>SIHANG QIU</strong></h1>
-				<!-- <p><a href="mailto:sihangq@acm.org"><span style="color:#336699">sihangq@acm.org</span></a></p> -->
-			</center>
-		</div>
-
-		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#e2e2e2">
-			<center>
-					<a href="mailto:sihangq@acm.org"><img src="./files/img/logos/email.png" height="30"/></a> &nbsp;
-					<a href="https://scholar.google.com/citations?hl=en&user=sDqDmq8AAAAJ&sortby=pubdate" target="_blank"><img src="./files/img/logos/google_scholar.png" height="30"/></a> &nbsp;
-					<a href="https://dblp.org/pid/210/5795.html" target="_blank"><img src="./files/img/logos/dblp.png" height="30"/></a> &nbsp;
-					<!-- <a href="https://www.researchgate.net/profile/Sihang_Qiu2" target="_blank"><img src="./files/img/logos/researchgate.png" height="30"/></a> &nbsp; -->
-					<!-- <a href="https://github.com/qiusihang" target="_blank"><img src="./files/img/logos/github.png" height="30"/></a> &nbsp; -->
-					<a href="https://twitter.com/qiusihang" target="_blank"><img src="./files/img/logos/twitter.png" height="30"/></a>
-			</center>
-		</div>
-
-		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#f2f2f2">
-			<center>
-				<table><tr>
-					<th width="100px"><a style="text-decoration:none" href="index.html">ABOUT</a></th>
-					<th width="150px"><a style="text-decoration:none" href="publications.html">PUBLICATIONS</a></th>
-				</tr></table>
-			</center>
-		</div>
-`;
+document.getElementById("header").innerHTML = '\
+		<div class="header" style="padding-top:20px; padding-bottom:5px; background:#d2d2d2">\
+			<!-- <img id="header-img" src="" width="800px"> -->\
+			<center>\
+				<img id="profile-img" src="./files/img/profile.jpg"></img>\
+				<h1><strong>SIHANG QIU</strong></h1>\
+				<!-- <p><a href="mailto:sihangq@acm.org"><span style="color:#336699">sihangq@acm.org</span></a></p> -->\
+			</center>\
+		</div>\
+		\
+		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#e2e2e2">\
+			<center>\
+					<a href="mailto:sihangq@acm.org"><img src="./files/img/logos/email.png" height="30"/></a> &nbsp;\
+					<a href="https://scholar.google.com/citations?hl=en&user=sDqDmq8AAAAJ&sortby=pubdate" target="_blank"><img src="./files/img/logos/google_scholar.png" height="30"/></a> &nbsp;\
+					<a href="https://dblp.org/pid/210/5795.html" target="_blank"><img src="./files/img/logos/dblp.png" height="30"/></a> &nbsp;\
+					<!-- <a href="https://www.researchgate.net/profile/Sihang_Qiu2" target="_blank"><img src="./files/img/logos/researchgate.png" height="30"/></a> &nbsp; -->\
+					<!-- <a href="https://github.com/qiusihang" target="_blank"><img src="./files/img/logos/github.png" height="30"/></a> &nbsp; -->\
+					<a href="https://twitter.com/qiusihang" target="_blank"><img src="./files/img/logos/twitter.png" height="30"/></a>\
+			</center>\
+		</div>\
+		\
+		<div class="header" style="padding-top:10px; padding-bottom:10px; background:#f2f2f2">\
+			<center>\
+				<table><tr>\
+					<th width="100px"><a style="text-decoration:none" href="index.html">ABOUT</a></th>\
+					<th width="150px"><a style="text-decoration:none" href="publications.html">PUBLICATIONS</a></th>\
+				</tr></table>\
+			</center>\
+		</div>\
+';
