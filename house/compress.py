49a4e963af498efbd2e5da65e5d7a0faa0f25f39
Sihang Qiu <33344405+qiusihang@users.noreply.github.com>
2023-08-30 00:00:26 +0800

    update versions

--- house/compress.py
+++ house/compress.py
@@ -4,8 +4,8 @@ import os
 for root, ds, fs in os.walk("./"):
     for f in fs:
         fullname = os.path.join(root, f)
-        if fullname.__contains__("_low.jpg"):
+        if fullname.__contains__(".jpg") and (not fullname.__contains__("_low.jpg")):
             image = Image.open(fullname)
-            resized = image.resize((256, 256))
-            # resized.save(fullname.replace(".jpg","_low.jpg"))
-            resized.save(fullname)
+            resized = image.resize((512, 512))
+            resized.save(fullname.replace(".jpg","_low.jpg"))
+            # resized.save(fullname)
