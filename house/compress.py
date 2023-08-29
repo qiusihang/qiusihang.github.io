5dec334cfac7a2576b49cbc82c2b22c9f46c0121
Sihang Qiu <33344405+qiusihang@users.noreply.github.com>
2023-08-29 23:39:38 +0800

    lower resolution

--- house/compress.py
+++ house/compress.py
@@ -4,7 +4,8 @@ import os
 for root, ds, fs in os.walk("./"):
     for f in fs:
         fullname = os.path.join(root, f)
-        if fullname.__contains__(".jpg"):
+        if fullname.__contains__("_low.jpg"):
             image = Image.open(fullname)
-            resized = image.resize((400, 400))
-            resized.save(fullname.replace(".jpg","_low.jpg"))
+            resized = image.resize((256, 256))
+            # resized.save(fullname.replace(".jpg","_low.jpg"))
+            resized.save(fullname)
